d86122ed4285bde8603be8bbb9f23361932ea428
Filippo Miorin <mezzasegolas@gmail.com>
2024-08-19 09:23:53 +0200

    refactor example payload

--- lib/examples/proof_of_concept_pow_captcha.dart
+++ lib/examples/proof_of_concept_pow_captcha.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:cryptography/cryptography.dart';
 import 'package:convert/convert.dart';
 import 'package:proof_of_work_verification/constants/string.dart';
+import 'package:proof_of_work_verification/models/challenge_payload_model.dart';
 
 // Expect a payload such as
 // {
@@ -19,33 +20,43 @@ import 'package:proof_of_work_verification/constants/string.dart';
 //  },
 //  "hash": "8ea09bf4f6ac1e0b60d3b70e5692821b"
 // }
+
+const challengePayload = ChallengePayloadModel(
+    id: "6c2cbad0-5626-485d-b9ed-08457a6da44a",
+    action: "whatever_you_put_in_request",
+    range: 100,
+    salt: "1234abcd",
+    hash: "8ea09bf4f6ac1e0b60d3b70e5692821b",
+    algorithm: Algorithm(
+        name: "argon2id",
+        memoryCost: 1024,
+        parallelism: 1,
+        iterations: 1,
+        hashLength: 16,
+    ),
+);
+
 Future<void> main() async {
-  // The following will be returned by MPS
-  const String id = '6c2cbad0-5626-485d-b9ed-08457a6da44a';
-  const int range = 100;
-  const String salt = '1234abcd';
-  // hash == salt + random number => argon2id()
-  const hash = '8ea09bf4f6ac1e0b60d3b70e5692821b';
 
   // Define algorithm -- should align with BE
   final Argon2id algorithm = Argon2id(
-    memory: 1024,
-    parallelism: 1, // Use maximum two CPU cores.
-    iterations: 1, // For more security, you should usually raise memory parameter, not iterations.
-    hashLength: 16,
+    memory: challengePayload.algorithm.memoryCost,
+    parallelism: challengePayload.algorithm.parallelism, // Use maximum two CPU cores.
+    iterations: challengePayload.algorithm.iterations, // For more security, you should usually raise memory parameter, not iterations.
+    hashLength: challengePayload.algorithm.hashLength,
   );
 
   final DateTime startTime = DateTime.now();
   // init PoW Captcha
   int nounce = 0;
   // run max - min n of iterations
-  while(nounce < range) {
+  while(nounce < challengePayload.range) {
 
     // Represents UTF-8 encoded nounce
     final Uint8List solutionBytes =
       utf8.encode('$nounce${StringConstants.modifier}');
     // Represents UTF-8 encoded salt
-    final Uint8List saltBytes = utf8.encode(salt);
+    final Uint8List saltBytes = utf8.encode(challengePayload.salt);
 
     // Calculate output of Argon2id algorithm
     final SecretKey newSecretKey = await algorithm.deriveKey(
@@ -58,7 +69,7 @@ Future<void> main() async {
     // convert into hex
     final String currentHashString = hex.encode(currentHashBytes);
 
-    if (currentHashString == hash) {
+    if (currentHashString == challengePayload.hash) {
       print('Success: return $nounce');
       break;
     }
